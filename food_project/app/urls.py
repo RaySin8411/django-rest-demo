b8fe2646462300c2b77dfbcb309e419aa01cec2c
Ray <q8977452@gmail.com>
2022-07-10 23:02:34 +0800

    feat(hello): add hello url

--- food_project/app/urls.py
+++ food_project/app/urls.py
@@ -1,4 +1,4 @@
-"""food_project URL Configuration
+"""app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', hello),
 ]
