90f190864903883d5f9a407b94097b7059800325
Ray <q8977452@gmail.com>
2022-07-11 11:18:17 +0800

    feat(food): Add CRUD api for food

--- food_project/food/views.py
+++ food_project/food/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Food
+from .serializers import FoodSerializer
+
 
 # Create your views here.
+
+class FoodListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializer
+
+
+class FoodDetailAPIview(generics.RetrieveUpdateDestroyAPIView):
+    queryset = FOod.objects.all()
+    serializer_class = FoodSerializer
